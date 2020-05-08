6d335fb2c3860d4b44b3348ce4772c6d011908bd
TanishqSingla <tanishqsingla00@gmail.com>
2020-05-08 23:48:00 +0530

    Connecting app with database
    Added mongodb to app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,21 +9,40 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [];
-let workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+});
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todolist!",
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add a new item",
+});
+
+const item3 = new Item({
+  name: "<-- Hit this button to delete an item",
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, (e) => {
+  if (e) {
+    console.log(e);
+  } else {
+    console.log("Successfully added to db");
+  }
+});
 
 app.get("/", (req, res) => {
-  let today = new Date();
-
-  let format = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", format);
-
-  res.render("list", { listTitle: day, newListItem: items });
+  res.render("list", { listTitle: "Today", newListItem: items });
 });
 
 app.post("/", (req, res) => {
